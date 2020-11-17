e21f321997a30d45c8c581862978b4635567a505
Richard Leung <michelleli@MMac.local>
2020-11-17 11:41:22 -0500

    create new dinosaur route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const layouts = require('express-ejs-layouts');
 
 const dinoRouter = require('./controllers/dinoController')
 
 app.set('view engine', 'ejs')
 app.use(layouts);
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req,res) => {
     res.render('dinosaurs/index')
