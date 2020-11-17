1697ca5312fe3e75753dd115cd7553472593bb5b
Richard Leung <michelleli@MMac.local>
2020-11-17 12:33:18 -0500

    cryptid routes

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const app = express()
 const layouts = require('express-ejs-layouts');
 
 const dinoRouter = require('./controllers/dinoController')
+const cryptidRouter = require('./controllers/cryptidController')
 
 app.set('view engine', 'ejs')
 app.use(layouts);
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req,res) => {
-    res.render('dinosaurs/index')
+    res.send('Hello!')
 })
 
 app.use('/dinosaurs', dinoRouter)
+app.use('/cryptids', cryptidRouter)
 
 app.listen(8000, () => {
     console.log('server started!')
